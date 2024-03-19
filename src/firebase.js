9e3f7e6f42225de5002f0df0fd8428dff147eacf
MQamar109 <qamar.mushtaq@cogentlabs.co>
2024-03-19 10:33:40 +0500

    comment resolved

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMtHtwTI3gaZ18_wLgUXbXJgm6JdOFQB0",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "contactlist-90842.firebaseapp.com",
   databaseURL: "https://contactlist-90842-default-rtdb.firebaseio.com",
   projectId: "contactlist-90842",
   storageBucket: "contactlist-90842.appspot.com",
   messagingSenderId: "495773300259",
-  appId: "1:495773300259:web:77f9c209c72ab9b919e396",
+  appId: FIREBASE_API_ID,
 };
 
 const app = initializeApp(firebaseConfig);
